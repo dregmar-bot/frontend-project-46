84fb0d8b3431c582ecc22ff70de6a9bae42555ba
Aleksandr Manujlo <dregmar@bk.ru>
2023-03-12 19:47:16 +0530

    add tests for nested objects

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,30 +1,82 @@
-import { test, expect } from '@jest/globals';
+import {
+  test, expect, beforeAll, beforeEach,
+} from '@jest/globals';
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 import findDiff from '../src/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
+let __filename;
+let __dirname;
+let getFixturePath;
+let result;
+
+beforeAll(() => {
+  __filename = fileURLToPath(import.meta.url);
+  __dirname = dirname(__filename);
+  getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
+});
+beforeEach(() => {
+  result = '{\n'
+    + '    common: {\n'
+    + '      + follow: false\n'
+    + '        setting1: Value 1\n'
+    + '      - setting2: 200\n'
+    + '      - setting3: true\n'
+    + '      + setting3: null\n'
+    + '      + setting4: blah blah\n'
+    + '      + setting5: {\n'
+    + '            key5: value5\n'
+    + '        }\n'
+    + '        setting6: {\n'
+    + '            doge: {\n'
+    + '              - wow: \n'
+    + '              + wow: so much\n'
+    + '            }\n'
+    + '            key: value\n'
+    + '          + ops: vops\n'
+    + '        }\n'
+    + '    }\n'
+    + '    group1: {\n'
+    + '      - baz: bas\n'
+    + '      + baz: bars\n'
+    + '        foo: bar\n'
+    + '      - nest: {\n'
+    + '            key: value\n'
+    + '        }\n'
+    + '      + nest: str\n'
+    + '    }\n'
+    + '  - group2: {\n'
+    + '        abc: 12345\n'
+    + '        deep: {\n'
+    + '            id: 45\n'
+    + '        }\n'
+    + '    }\n'
+    + '  + group3: {\n'
+    + '        deep: {\n'
+    + '            id: {\n'
+    + '                number: 45\n'
+    + '            }\n'
+    + '        }\n'
+    + '        fee: 100500\n'
+    + '    }\n'
+    + '}';
+});
 
 test('findDiff in 2 jsons', () => {
   const pathToJson1 = getFixturePath('file1.json');
   const pathToJson2 = getFixturePath('file2.json');
-  const result = `{\n - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true
-}`;
-  expect(findDiff(pathToJson1, pathToJson2)).toBe(result);
+  const diff = findDiff(pathToJson1, pathToJson2);
+  expect(diff).toStrictEqual(result);
 });
-test('findDiff in 2 yamls', () => {
+test('findDiff in yml and yaml', () => {
   const pathToYml1 = getFixturePath('file1.yml');
   const pathToYml2 = getFixturePath('file2.yaml');
-  const result = `{\n - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true
-}`;
-  expect(findDiff(pathToYml1, pathToYml2)).toBe(result);
+  const diff = findDiff(pathToYml1, pathToYml2);
+  expect(diff).toBe(result);
 });
 test('findDiff in yml and json', () => {
   const pathToYml1 = getFixturePath('file1.yml');
   const pathToJson2 = getFixturePath('file2.json');
-  const result = `{\n - follow: false\n   host: hexlet.io\n - proxy: 123.234.53.22\n - timeout: 50\n + timeout: 20\n + verbose: true
-}`;
-  expect(findDiff(pathToYml1, pathToJson2)).toBe(result);
+  const diff = findDiff(pathToYml1, pathToJson2);
+  expect(diff).toBe(result);
 });
