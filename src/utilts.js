942c1d12196ef71aff7750f6cbe533934bc12683
Aleksandr Manujlo <dregmar@bk.ru>
2023-03-22 14:57:34 +0530

    refactor utilts.js

--- src/utilts.js
+++ src/utilts.js
@@ -2,19 +2,7 @@ import _ from 'lodash';
 
 const getValue = (obj, key) => obj[key];
 const isObject = (value) => _.isObjectLike(value) && !Array.isArray(value);
-const findValue = (item) => {
-  const { status, oldValue, newValue } = item;
-  switch (status) {
-    case 'removed':
-    case 'unchanged':
-      return oldValue;
-    case 'added':
-      return newValue;
-    default:
-      throw new Error(`Unexpected status ${status}`);
-  }
-};
 
 export {
-  getValue, isObject, findValue,
+  getValue, isObject,
 };
