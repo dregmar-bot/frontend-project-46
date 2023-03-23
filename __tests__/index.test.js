c84d819223596092aede7a192c86c58f28026866
Aleksandr Manujlo <dregmar@bk.ru>
2023-03-23 19:13:54 +0530

    refactor index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -17,7 +17,7 @@ test.each([
     name1: 'file1.json', name2: 'file2.json', result: stylishResult, formatter: 'stylish',
   },
   {
-    name1: 'file1.yml', name2: 'file2.yaml', result: stylishResult,
+    name1: 'file1.yml', name2: 'file2.yaml', result: stylishResult, formatter: 'default',
   },
   {
     name1: 'file1.yml', name2: 'file2.json', result: plainResult, formatter: 'plain',
@@ -26,10 +26,10 @@ test.each([
     name1: 'file1.yml', name2: 'file2.yaml', result: jsonResult, formatter: 'json',
   },
 ])('diff between $name1 and $name2 in $formatter format', ({
-  name1, name2, result, formatter = 'stylish',
+  name1, name2, result, formatter,
 }) => {
   const path1 = getFixturePath(name1);
   const path2 = getFixturePath(name2);
-  const diff = genDiff(path1, path2, formatter);
+  const diff = formatter === 'default' ? genDiff(path1, path2) : genDiff(path1, path2, formatter);
   expect(diff).toStrictEqual(result);
 });
