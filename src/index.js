edf77424c6c7b99300abeb078a878a76cb6b21ca
Aleksandr Manujlo <dregmar@bk.ru>
2023-04-03 08:19:29 +0600

    refactor index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import parseData from './parsers.js';
 import buildTree from './buildTree.js';
 import makeFormatting from './formatters/index.js';
 
-const getType = (filepath) => filepath.split('.').reverse()[0];
+const getType = (filepath) => filepath.split('.').at(-1);
 const genDiff = (path1, path2, formatter = 'stylish') => {
   const data1 = parseData(readFileSync(path1), getType(path1));
   const data2 = parseData(readFileSync(path2), getType(path2));
