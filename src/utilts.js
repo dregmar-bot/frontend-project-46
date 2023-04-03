51c98be9ccc2dbc8ff28c09410c80d56f25efdfd
Aleksandr Manujlo <dregmar@bk.ru>
2023-04-03 08:13:32 +0600

    remove utilts.js

--- src/utilts.js
+++
@@ -1,8 +0,0 @@
-import _ from 'lodash';
-
-const getValue = (obj, key) => obj[key];
-const isObjectExceptArray = (value) => _.isObjectLike(value) && !Array.isArray(value);
-
-export {
-  getValue, isObjectExceptArray,
-};
