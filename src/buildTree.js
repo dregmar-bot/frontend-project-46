73895d617a792b7c0c0a1bc5fe2b7d3851dacdd5
Aleksandr Manujlo <dregmar@bk.ru>
2023-04-03 08:12:26 +0600

    refactor buildTree.js

--- src/buildTree.js
+++ src/buildTree.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
-import { getValue, isObjectExceptArray } from './utilts.js';
 
+const getValue = (obj, key) => obj[key];
 const buildTree = (obj1, obj2) => {
   const keys1 = Object.keys(obj1);
   const keys2 = Object.keys(obj2);
@@ -8,9 +8,9 @@ const buildTree = (obj1, obj2) => {
   return keys.map((key) => {
     const oldValue = getValue(obj1, key);
     const newValue = getValue(obj2, key);
-    if (isObjectExceptArray(getValue(obj1, key)) && isObjectExceptArray(getValue(obj2, key))) {
+    if (_.isPlainObject(getValue(obj1, key)) && _.isPlainObject(getValue(obj2, key))) {
       const children = buildTree(oldValue, newValue);
-      return { name: key, status: 'unchanged', children };
+      return { name: key, status: 'nested', children };
     }
     if (_.isEqual(getValue(obj1, key), getValue(obj2, key))) {
       return { name: key, status: 'unchanged', oldValue };
@@ -23,10 +23,7 @@ const buildTree = (obj1, obj2) => {
     if (_.has(obj1, key)) {
       return { name: key, status: 'removed', oldValue };
     }
-    if (_.has(obj2, key)) {
-      return { name: key, status: 'added', newValue };
-    }
-    throw new Error(`The key ${key} is missing in all objects`);
+    return { name: key, status: 'added', newValue };
   });
 };
 
